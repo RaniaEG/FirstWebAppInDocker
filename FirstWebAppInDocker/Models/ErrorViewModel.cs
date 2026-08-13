be131846259180533ac7aeee9c0cb1c1be5ee5a3
Rania Fahim Hassan Ibrahim Elgazzar <rania.elgazzar@gmail.com>
2026-08-13 15:28:53 +0200

    Add project files.

---
+++ FirstWebAppInDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstWebAppInDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
